0418962a9ff9937f39a23ae46c6b7aaf42bae38c
Stanislav <rus.abaaac@gmail.com>
2022-12-11 23:55:57 +0600

    init

---
+++ my-app/pages/index.js
@@ -0,0 +1,170 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { Contract, providers } from "ethers";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants';
+
+
+
+export default function Home() {
+
+  const [numOfWhitelisted, setNumOfWhitelisted] = useState(0);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+
+      const { chainId } = await web3Provider.getNetwork();
+      if (chainId !== 5) {
+        window.alert("change the network to Goerli!");
+        throw new Error("change the network to Goerli");
+      }
+
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+
+      return web3Provider;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+
+  const checkIfAddressInWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const address = await signer.getAddress();
+      const _joinedWhitelist = await whitelistContract.whitelistedAddresses(address);
+      setJoinedWhitelist(_joinedWhitelist);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const getNumberOfWhitelisted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      const _numOfWhitelisted = await whitelistContract.numAddressesWhitelisted();
+      setNumOfWhitelisted(_numOfWhitelisted);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+
+  const addAddressToWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const tx = await whitelistContract.addAddressToWhitelist();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      await getNumberOfWhitelisted();
+      setJoinedWhitelist(true);
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedWhitelist) {
+        return (
+          <div className={styles.description}>
+            Thank for joining the Whitelist!
+          </div>
+        );
+      } else if (loading) {
+        return (<button className={styles.button}>
+          Loading...
+        </button>
+        );
+      } else {
+        return (<button onClick={addAddressToWhitelist} className={styles.button}>
+          Join the Whitelist!
+        </button>
+        );
+      }
+    } else {
+      return (<button onClick={connectWallet} className={styles.button}>
+        Connect wallet
+      </button>
+      );
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+      checkIfAddressInWhitelist();
+      getNumberOfWhitelisted();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
+  return (
+    <div>
+      <Head>
+        <title>
+          Whitelist-Dapp
+        </title>
+        <meta name="description" content="Whitelist-Dapp" />
+      </Head>
+
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}> Welcome to be the part of DAO! </h1>
+          <div className={styles.description}> It's NFT collection. </div>
+          <div className={styles.description}> {numOfWhitelisted} have already joined. </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#9829;
+      </footer>
+
+    </div>
+  )
+}
